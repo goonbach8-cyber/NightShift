04ce88874cd4ca409696e6eb0e268571221c1fa4
goonbach8-cyber <goonbach8@gmail.com>
2026-09-09 15:10:48 +0200

    Use AnimatedSprite3D for player animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,22 +6,13 @@ extends CharacterBody3D
 @export_range(0.1, 50.0, 0.1) var deceleration: float = 24.0
 @export_range(0.1, 50.0, 0.1) var gravity: float = 20.0
 
-@export_group("Animation")
-@export_range(1.0, 20.0, 0.5) var walk_animation_fps: float = 10.0
+@onready var sprite: AnimatedSprite3D = $AnimatedSprite3D
 
-@onready var sprite: Sprite3D = $Sprite3D
-
-# Sprite-sheet rows:
-# 0 = front/down, 1 = right, 2 = left, 3 = back/up
-const FRAMES_PER_DIRECTION: int = 8
-
-var facing_row: int = 0
-var animation_frame: int = 0
-var animation_timer: float = 0.0
+var facing: StringName = &"down"
 
 
 func _ready() -> void:
-	_apply_sprite_frame()
+	sprite.play(&"idle_down")
 
 
 func _physics_process(delta: float) -> void:
@@ -44,31 +35,28 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y -= gravity * delta
 
-	_update_sprite_animation(move_direction, delta)
+	_update_facing(move_direction)
+	_update_animation(move_direction)
 	move_and_slide()
 
 
-func _update_sprite_animation(move_direction: Vector3, delta: float) -> void:
+func _update_facing(move_direction: Vector3) -> void:
 	if move_direction.length_squared() < 0.0025:
-		animation_frame = 0
-		animation_timer = 0.0
-		_apply_sprite_frame()
 		return
 
 	if absf(move_direction.x) > absf(move_direction.z):
-		facing_row = 1 if move_direction.x > 0.0 else 2
+		facing = &"right" if move_direction.x > 0.0 else &"left"
 	else:
-		facing_row = 0 if move_direction.z > 0.0 else 3
-
-	animation_timer += delta
-	var frame_duration := 1.0 / walk_animation_fps
-
-	while animation_timer >= frame_duration:
-		animation_timer -= frame_duration
-		animation_frame = (animation_frame + 1) % FRAMES_PER_DIRECTION
-
-	_apply_sprite_frame()
+		facing = &"down" if move_direction.z > 0.0 else &"up"
 
 
-func _apply_sprite_frame() -> void:
-	sprite.frame = facing_row * FRAMES_PER_DIRECTION + animation_frame
+func _update_animation(move_direction: Vector3) -> void:
+	var animation_name: StringName
+
+	if move_direction.length_squared() < 0.0025:
+		animation_name = StringName("idle_" + String(facing))
+	else:
+		animation_name = StringName("walk_" + String(facing))
+
+	if sprite.animation != animation_name:
+		sprite.play(animation_name)
